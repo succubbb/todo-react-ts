be37352d3675fbc6073018915fd74dc266379e33
Маргарита Телкова <n5212549@ya.ru>
2026-06-24 16:26:21 +0300

    initial application setup

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './style.css';
+import App from './App.tsx';
+
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+  throw new Error('Root element not found');
+}
+
+createRoot(rootElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
