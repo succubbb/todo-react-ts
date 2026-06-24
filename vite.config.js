7d95c0c49222743ec797d1af6365df31e011895f
Маргарита Телкова <n5212549@ya.ru>
2026-06-24 13:50:27 +0300

    initial application setup

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite'
-
+import tailwindcss from '@tailwindcss/vite';
+import { ViteFaviconsPlugin } from 'vite-plugin-favicon';
 
 export default defineConfig({
-    plugins: [
-        react(),
-        tailwindcss(),
-    ],
+  plugins: [
+    react(),
+    tailwindcss(),
+    ViteFaviconsPlugin({
+      logo: 'public/icons/logo.png',
+      inject: true,
+    }),
+  ],
+  server: {
+    open: true, // Автоматически открывает браузер при старте
+    port: 3000, // Порт (по умолчанию 5173)
+    host: true, // Открывает доступ по локальной сети (опционально)
+  },
 });
